c0b5ab8e0e22ea511af272db656cb0c8896ec802
Herbert Izidro <herbertsouza80@yahoo.com>
2022-03-02 12:07:12 -0300

    instrução adc na index

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,8 @@
 
 export default function Home() {
   return (
-    <p></p>
+    <div style={{padding: 20, margin: 20}}>
+      <p>MovieInfoApi<br/>Hello. To make a query, go to https://movieinfoapi.netlify.app/api/movieinfo?search=<<movie+name>></p>
+    </div>
   )
 }
