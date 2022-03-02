d49ff771b5d8b143e9e375b8f8d78c62567a45b5
Herbert Izidro <herbertsouza80@yahoo.com>
2022-03-02 12:19:39 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@
 export default function Home() {
   return (
     <div style={{padding: 20, margin: 20}}>
-      <p>MovieInfoApi<br/>Hello. To make a query, go to https://movieinfoapi.netlify.app/api/movieinfo?search=<<movie+name>></p>
+      <p>MovieInfoApi<br/>Hello. To make a query, go to https://movieinfoapi.netlify.app/api/movieinfo?search=movie+name</p>
     </div>
   )
 }
